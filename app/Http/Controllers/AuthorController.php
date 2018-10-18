5bdbfa8b0902fb7df24f9ff0d0af21687d252308
MaryAnne45 <mary.jereh@gmail.com>
2018-10-18 18:57:55 +0300

    valiate any user who is making any pos request

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -3,37 +3,52 @@
 namespace App\Http\Controllers;
 
 use App\Author;
+
 use Illuminate\Http\Request;
 /*
-we require the author model 
+we require the author model
 we have five methods
 we return the response in json format
 In the  first function we get all the authors details
 */
 class AuthorController extends Controller
 {
-  public function showAllAuthors()
-  {
-    return response()->json(Author::all());
-  }
-  public function showOneAuthor()
-  {
-    return response()->json(Author::find($id));
-  }
-  public function create (Request $request)
-  {
-    $author = Author::create($request->all());
-    return response()->json($author, 201);
-  }
-  public funciton update($id ,Request $request)
-  {
-    $author = Author::findorFail($id);
-    $author->update($request->all());
-    return response()->json($author,200);
-  }
-  public function delete($id)
-  {
-    Author::findorFail($id)->delete();
-    return response('Deleted succefully',200);
-  }
+
+    public function showAllAuthors()
+    {
+        return response()->json(Author::all());
+    }
+
+    public function showOneAuthor($id)
+    {
+        return response()->json(Author::find($id));
+    }
+    /*
+    we validate any user that wants to send data to the the api
+    */
+    public function create(Request $request)
+    {
+      $this->validate($request, [
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'location' => 'required|alpha'
+      ]);
+        $author = Author::create($request->all());
+
+        return response()->json($author, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $author = Author::findOrFail($id);
+        $author->update($request->all());
+
+        return response()->json($author, 200);
+    }
+
+    public function delete($id)
+    {
+        Author::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
 }
